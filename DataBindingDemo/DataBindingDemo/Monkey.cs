7562ed7002e2df774e54afa5dd933ceea04a67d2
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-05-30 02:21:57 -0500

    Agregar archivos de proyecto.

---
+++ DataBindingDemo/DataBindingDemo/Monkey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBindingDemo
+{
+  
+        public class Monkey
+        {
+            public string Name { get; set; }
+            public string Location { get; set; }
+            public string Details { get; set; }
+            public string ImageUrl { get; set; }
+        }
+  
+}
